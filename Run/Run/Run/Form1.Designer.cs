00d750c4da503d55493eeffbe361374962b60504
MykytaLavrov <nikitalavrov93@gmail.com>
2022-10-31 13:39:29 +0200

    Arduino
    Arduino Ultrasonic and etc.

---
+++ Run/Run/Run/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Run
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(481, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "START";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "W";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(504, 217);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 48);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "S";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(565, 168);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(59, 48);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "D";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(418, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Ultrasonic sensor";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(543, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Buffer";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(481, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(99, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "10 cm";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(481, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 43);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(439, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 48);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "A";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(611, 396);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 20);
+            this.button5.TabIndex = 15;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 428);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FIRE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Run/Run/Run/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Run
+{
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawGrap postroenie = new DrawGrap(panel1.CreateGraphics());
+            postroenie.Postroenie();
+            postroenie.Dispose();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random randomNumbers = new Random();
+            int fireNumber = randomNumbers.Next(1, 8);
+            DrawGrap fire = new DrawGrap(panel1.CreateGraphics());
+            fire.Fire(fireNumber);
+            fire.Dispose();
+
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+          //  Form1 MyFrm2 = new Form1();
+           // Point point = new Point(Location.X, Location.Y);
+            int X = Location.X;
+            int Y = Location.Y;
+
+            // Point W = Run.Form1.Location;
+             Color clofxdv = Win32.GetPixelColor(100,100);
+             
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+       
+
+
+        public class DrawGrap : Form1
+        {
+            protected Graphics GG;
+
+            public DrawGrap(Graphics g)
+            {
+                GG = g;
+            }
+
+
+            public void Postroenie()
+            {
+                Pen P = new Pen(System.Drawing.Color.Black);
+                Pen Door = new Pen(System.Drawing.Color.Black, 3);
+
+
+                GG.DrawRectangle(P, 0, 0, 399, 399);
+
+                GG.DrawLine(P, 100, 100, 300, 100);
+                GG.DrawLine(P, 300, 0, 300, 400);
+                GG.DrawLine(P, 300, 200, 400, 200);
+                GG.DrawLine(P, 300, 300, 400, 300);
+                GG.DrawLine(P, 300, 200, 400, 200);
+                GG.DrawLine(P, 0, 200, 200, 200);
+                GG.DrawLine(P, 200, 200, 200, 400);
+                GG.DrawLine(P, 300, 200, 400, 200);
+                GG.DrawLine(P, 0, 300, 200, 300);
+                GG.DrawLine(P, 100, 0, 100, 200);
+                P.Dispose();
+
+                System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 16);
+
+                GG.FillEllipse(Brushes.Black, 89, 340, 26, 26);
+                GG.FillEllipse(Brushes.White, 90, 341, 24, 24);
+                GG.DrawString("1", drawFont, Brushes.Black, 92, 342);
+
+                GG.FillEllipse(Brushes.Black, 89, 240, 26, 26);
+                GG.FillEllipse(Brushes.White, 90, 241, 24, 24);
+                GG.DrawString("2", drawFont, Brushes.Black, 92, 242);
+
+                GG.FillEllipse(Brushes.Black, 39, 90, 26, 26);
+                GG.FillEllipse(Brushes.White, 40, 91, 24, 24);
+                GG.DrawString("3", drawFont, Brushes.Black, 42, 92);
+
+                GG.FillEllipse(Brushes.Black, 189, 40, 26, 26);
+                GG.FillEllipse(Brushes.White, 190, 41, 24, 24);
+                GG.DrawString("4", drawFont, Brushes.Black, 192, 42);
+
+                GG.FillEllipse(Brushes.Black, 339, 40, 26, 26);
+                GG.FillEllipse(Brushes.White, 340, 41, 24, 24);
+                GG.DrawString("5", drawFont, Brushes.Black, 342, 42);
+
+                GG.FillEllipse(Brushes.Black, 339, 240, 26, 26);
+                GG.FillEllipse(Brushes.White, 340, 241, 24, 24);
+                GG.DrawString("6", drawFont, Brushes.Black, 342, 242);
+
+                GG.FillEllipse(Brushes.Black, 339, 339, 26, 26);
+                GG.FillEllipse(Brushes.White, 340, 340, 24, 24);
+                GG.DrawString("7", drawFont, Brushes.Black, 342, 342);
+
+                drawFont.Dispose();
+
+
+
+                GG.DrawLine(Door, 230, 398, 270, 398);
+                GG.DrawLine(Door, 100, 130, 100, 170);
+                GG.DrawLine(Door, 300, 130, 300, 170);
+                GG.DrawLine(Door, 200, 230, 200, 270);
+                GG.DrawLine(Door, 300, 230, 300, 270);
+                GG.DrawLine(Door, 200, 330, 200, 370);
+                GG.DrawLine(Door, 300, 330, 300, 370);
+                GG.DrawLine(Door, 130, 100, 170, 100);
+                Door.Dispose();
+
+                GG.FillRectangle(Brushes.Purple, 240, 370, 20, 25);
+              //  GG.FillRectangle(Brushes.Purple, 0, 0, 400, 400);
+
+
+
+              /*        for (int i = 370; i > 0; --i)
+                      {
+                          Thread.Sleep(10);
+                          GG.FillRectangle(Brushes.Purple, 240, i, 20, 25);
+                      }*/
+
+
+                //   GG.Dispose();
+
+            }
+
+            public void Fire(int fireNumber)
+            {
+                System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 16);
+
+                if (fireNumber == 1)
+                {
+                    GG.FillEllipse(Brushes.Black, 89, 340, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 90, 341, 24, 24);
+                    GG.DrawString("1", drawFont, Brushes.Black, 92, 342);
+
+                }
+                else if (fireNumber == 2)
+                {
+                    GG.FillEllipse(Brushes.Black, 89, 240, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 90, 241, 24, 24);
+                    GG.DrawString("2", drawFont, Brushes.Black, 92, 242);
+                }
+                else if (fireNumber == 3)
+                {
+                    GG.FillEllipse(Brushes.Black, 39, 90, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 40, 91, 24, 24);
+                    GG.DrawString("3", drawFont, Brushes.Black, 42, 92);
+
+                }
+                else if (fireNumber == 4)
+                {
+                    GG.FillEllipse(Brushes.Black, 189, 40, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 190, 41, 24, 24);
+                    GG.DrawString("4", drawFont, Brushes.Black, 192, 42);
+                }
+                else if (fireNumber == 5)
+                {
+                    GG.FillEllipse(Brushes.Black, 339, 40, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 340, 41, 24, 24);
+                    GG.DrawString("5", drawFont, Brushes.Black, 342, 42);
+                }
+                else if (fireNumber == 6)
+                {
+                    GG.FillEllipse(Brushes.Black, 339, 240, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 340, 241, 24, 24);
+                    GG.DrawString("6", drawFont, Brushes.Black, 342, 242);
+                }
+                else if (fireNumber == 7)
+                {
+                    GG.FillEllipse(Brushes.Black, 339, 339, 26, 26);
+                    GG.FillEllipse(Brushes.Red, 340, 340, 24, 24);
+                    GG.DrawString("7", drawFont, Brushes.Black, 342, 342);
+                }
+
+                drawFont.Dispose();
+            }
+
+            
+
+        }
+
+        public class Win32
+        {
+            [DllImport("user32.dll")]
+            static extern IntPtr GetDC(IntPtr hwnd);
+
+            [DllImport("user32.dll")]
+            static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+            [DllImport("gdi32.dll")]
+            static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+            static public System.Drawing.Color GetPixelColor(int x, int y)
+            {
+                IntPtr hdc = GetDC(IntPtr.Zero);
+                uint pixel = GetPixel(hdc, x, y);
+                ReleaseDC(IntPtr.Zero, hdc);
+                Color color = Color.FromArgb((int)(pixel & 0x000000FF),
+                             (int)(pixel & 0x0000FF00) >> 8,
+                             (int)(pixel & 0x00FF0000) >> 16);
+                return color;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Random randomNumbers = new Random();
+            int fireNumber = randomNumbers.Next(1, 8);
+            DrawGrap fire = new DrawGrap(panel1.CreateGraphics());
+            fire.Fire(fireNumber);
+            fire.Dispose();
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            DrawGrap postroenie = new DrawGrap(panel1.CreateGraphics());
+            postroenie.Postroenie();
+            postroenie.Dispose();
+
+        }
+    }
+}
